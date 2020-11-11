89fc816759e4faf88fd278e30d9889b84411550e
Wesley <wspencer3295@yahoo.com>
2020-11-11 15:13:48 -0700

    commit tree 14a6a75f97c1a7df59ec8c550de982b81d8d1537

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
-def oxford_comma(array)
 
-end
+
+
+
+    def oxford_comma(list)
+        return list.join(' and ') if list.size < 3
+        list[-1] = "and " + list[-1]
+        list.join(', ')
+      end
